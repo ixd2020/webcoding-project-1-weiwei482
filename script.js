56ec08cdd5b56e9e2f8b4d99940ba9e7d50dfa90
Glitch (assignment-1-vincent) <none>
2020-12-16 14:06:16 +0000

    Complete website

--- script.js
+++ script.js
@@ -15,12 +15,31 @@ anime.timeline({loop: false})
     opacity: [0,1],
     translateZ: 0,
     easing: "easeOutExpo",
-    duration: 950,
+    duration: 2000,
     delay: (el, i) => 70*i
   }).add({
     targets: '.ml2',
     opacity: 1,
-    duration: 1000,
+    duration: 1500,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml9 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml9 .letter',
+    scale: [0, 1],
+    duration: 1500,
+    elasticity: 600,
+    delay: (el, i) => 45 * (i+1)
+  }).add({
+    targets: '.ml9',
+    opacity: 1,
+    duration: 2000,
     easing: "easeOutExpo",
     delay: 1000
   });
